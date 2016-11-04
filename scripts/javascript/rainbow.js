528550df94dd0002250536e761bfde9d7e0b740f
Luke Evers <luke@lukevers.com>
2016-11-04 09:40:32 -0400

    Support numbers in rainbow.js
    Also reformat it a bit

--- scripts/javascript/rainbow.js
+++ scripts/javascript/rainbow.js
@@ -19,16 +19,16 @@ function rainbow(str) {
             }
         }
 
-        return '\x03' + count + char;      
+        return '\x03' + count + ',99' + char;
     });
 }
 
 if (irc.event.command == 'PRIVMSG') {
-	if (irc.event.message.indexOf('| rainbow') > -1) {
-		var message = rainbow(irc.event.message.replace('| rainbow', '').replace(/\s+/g, ' '));
-		irc.say(irc.event.channel, message);
-	} else if (irc.event.message.indexOf('@rainbow') > -1) {
-		var message = rainbow(irc.event.message.replace('@rainbow', '').replace(/\s+/g, ' '));
-		irc.say(irc.event.channel, message.trim());
-	}
+    if (irc.event.message.indexOf('| rainbow') > -1) {
+        var message = rainbow(irc.event.message.replace('| rainbow', '').replace(/\s+/g, ' '));
+        irc.say(irc.event.channel, message);
+    } else if (irc.event.message.indexOf('@rainbow') > -1) {
+        var message = rainbow(irc.event.message.replace('@rainbow', '').replace(/\s+/g, ' '));
+        irc.say(irc.event.channel, message.trim());
+    }
 }
